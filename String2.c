349e5b6c78bd1fa8a165af73ecdfd1e012950e21
karimovdjakhangir <93442903+karimovdjakhangir@users.noreply.github.com>
2021-11-13 09:43:32 -0500

    Add files via upload

---
+++ String2.c
@@ -0,0 +1,12 @@
+//String2
+
+#include <stdio.h>
+int main() {  
+    int a;
+    printf("Enter ASCII value of a character: ");
+    scanf("%d", &a);  
+    
+    printf("The character of %d ASCII value is = %c\n", a, a);
+    
+    return 0;
+}
