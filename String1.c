9a3fdaacbc6b286a01ccdaa92fab3349ce348f0f
karimovdjakhangir <93442903+karimovdjakhangir@users.noreply.github.com>
2021-11-13 09:30:24 -0500

    Add files via upload

---
+++ String1.c
@@ -0,0 +1,12 @@
+//String1
+
+#include <stdio.h>
+int main() {  
+    char a;
+    printf("Enter a character: ");
+    scanf("%a", &a);  
+    
+    printf("ASCII value of %c = %d\n", a, a);
+    
+    return 0;
+}
